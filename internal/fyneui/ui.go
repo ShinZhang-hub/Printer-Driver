915cf28744a72b5ed31137483af4456368da5087
ShinZhang <v_shinzhang@global.tencent.com>
2026-07-15 15:30:00 +0900

    fyneui: macOS-inspired layout, summary header, tighter spacing, cancel icon

--- internal/fyneui/ui.go
+++ internal/fyneui/ui.go
@@ -6,6 +6,7 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 	"printer-installer/internal/i18n"
 )
@@ -34,8 +35,13 @@ func Run(detectedLoc string, allLocations []string, deletePrinters []string, pri
 		}
 	}
 
-	confirmCheck := widget.NewCheck(i18n.T("CONFIRM_FMT", detectedLoc), func(on bool) {
-	})
+	// Summary label
+	summaryText := detectedLoc + "  |  " + i18n.T("LOCATION_PREFIX", "") + "  |  IP: TODO"
+	summaryLabel := widget.NewLabel(summaryText)
+	summaryLabel.Alignment = fyne.TextAlignCenter
+
+	// Section 1: Confirm
+	confirmCheck := widget.NewCheck(i18n.T("CONFIRM_FMT", detectedLoc), func(on bool) {})
 	confirmCheck.SetChecked(detectedLoc != "")
 	if detectedLoc == "" {
 		confirmCheck.Hide()
@@ -46,17 +52,19 @@ func Run(detectedLoc string, allLocations []string, deletePrinters []string, pri
 		locSelect.Hide()
 	} else if len(otherLocs) > 0 {
 		locSelect.SetSelected(otherLocs[0])
-		locSelect.Show()
 	} else {
 		locSelect.PlaceHolder = i18n.T("NO_LOCATION")
 	}
 
+	// Section 2: Conflict
 	skipT := i18n.T("SKIP_BTN")
 	overwriteT := i18n.T("OVERWRITE_LABEL")
 	conflictSelect := widget.NewSelect([]string{skipT, overwriteT}, func(s string) {})
 	conflictSelect.SetSelected(skipT)
 
-	// Build set of all installed printer IPs
+	conflictLabel := widget.NewLabel(i18n.T("CONFLICT_LABEL"))
+
+	// Section 3: Delete list
 	installedIPs := make(map[string]bool, len(printersIPs))
 	for _, ip := range printersIPs {
 		installedIPs[ip] = true
@@ -69,6 +77,7 @@ func Run(detectedLoc string, allLocations []string, deletePrinters []string, pri
 		delChecks = append(delChecks, cb)
 		delList.Add(cb)
 	}
+	delHeader := widget.NewLabel(i18n.T("EXISTING_PRINTERS", len(deletePrinters)))
 
 	updateDisabled := func(loc string) {
 		ips := locIPs[loc]
@@ -137,37 +146,37 @@ func Run(detectedLoc string, allLocations []string, deletePrinters []string, pri
 		w.Close()
 	})
 
-	cancelBtn := widget.NewButton(i18n.T("CANCEL_LABEL"), func() {
+	cancelBtn := widget.NewButtonWithIcon("", theme.CancelIcon(), func() {
 		result = &Result{Cancelled: true}
 		w.Close()
 	})
 
-	topFixed := container.NewVBox(
-		widget.NewLabelWithStyle(i18n.T("TITLE"), fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
+	// Build layout matching macOS JXA structure
+	top := container.NewVBox(
+		container.NewPadded(summaryLabel),
 		widget.NewSeparator(),
 		confirmCheck,
-		locSelect,
+		container.NewPadded(locSelect),
 		widget.NewSeparator(),
-		widget.NewLabelWithStyle(i18n.T("CONFLICT_LABEL"), fyne.TextAlignLeading, fyne.TextStyle{Bold: true}),
-		conflictSelect,
+		conflictLabel,
+		container.NewPadded(conflictSelect),
 		widget.NewSeparator(),
-		widget.NewLabelWithStyle(i18n.T("EXISTING_PRINTERS", len(deletePrinters)), fyne.TextAlignLeading, fyne.TextStyle{Bold: true}),
+		delHeader,
 	)
 
-	inner := container.NewBorder(
-		topFixed, nil, nil, nil,
-		container.NewScroll(delList),
+	btnBox := container.NewHBox(
+		cancelBtn,
+		widget.NewSeparator(),
+		installBtn,
 	)
 
 	content := container.NewBorder(
-		nil,
-		container.NewHBox(container.NewCenter(), installBtn, cancelBtn),
-		nil, nil,
-		inner,
+		top, btnBox, nil, nil,
+		container.NewScroll(delList),
 	)
 
 	w.SetContent(container.NewPadded(content))
-	w.Resize(fyne.NewSize(480, 520))
+	w.Resize(fyne.NewSize(500, 480))
 	w.SetFixedSize(true)
 	w.CenterOnScreen()
 	w.ShowAndRun()
